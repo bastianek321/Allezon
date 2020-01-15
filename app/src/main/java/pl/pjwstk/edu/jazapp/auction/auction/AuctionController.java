51d0ff1f7d58a055a159d443b9b67e09c50848db
bastianek321 <scccghostman@gmail.com>
2020-01-15 17:28:17 +0100

    param added

--- app/src/main/java/pl/pjwstk/edu/jazapp/auction/auction/AuctionController.java
+++ app/src/main/java/pl/pjwstk/edu/jazapp/auction/auction/AuctionController.java
@@ -3,19 +3,12 @@ package pl.pjwstk.edu.jazapp.auction.auction;
 
 
 import pl.pjwstk.edu.jazapp.auction.entities.Auction;
-import pl.pjwstk.edu.jazapp.auction.entities.Category;
 import pl.pjwstk.edu.jazapp.auction.entities.Photo;
-import pl.pjwstk.edu.jazapp.users.SessionAllezon;
+import pl.pjwstk.edu.jazapp.utils.ParamRetriever;
 
 import javax.enterprise.context.RequestScoped;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 @Named
@@ -27,6 +20,8 @@ public class AuctionController {
     AuctionRequest auctionRequest;
     @Inject
     AuctionRepository auctionRepository;
+    @Inject
+    ParamRetriever paramRetriever;
 
 
     public void add(){
@@ -38,25 +33,16 @@ public class AuctionController {
         System.out.println("Added auction: "+auction.getTitle()+ " "+auction.getId());
     }
 
-
-//    public Auction showAuction(){
-//        HttpServletRequest req = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
-//        HttpServletResponse res = (HttpServletResponse)FacesContext.getCurrentInstance().getExternalContext().getResponse();
-//        return auctionRepository.getAuctionById(req.getParameter("auction"));
-//    }
-
     public List<Auction> getAllAuctions(){ return auctionRepository.getAllAuctions();}
 
     public List<Auction> getMyAuctions() {return  auctionRepository.getAuctionsByUser(creatorService.getOwner().getUsername());}
 
-    public Auction getAuction(Long id) {return auctionRepository.getAuctionById(id);}
+    public Auction getAuction() {
+        if(paramRetriever.contains("auction")){
+            return auctionRepository.getAuctionById(paramRetriever.getLong("auction"));
+        }
+        else return null;
 
-//    public void edit(Long id){
-//        Auction auction = getAuction(id);
-//        auction.setCategory(auctionRepository.getCategoryById(auctionRequest.getCategoryId()));
-//        auction.setDescription(auctionRequest.getDescription());
-//        auction.setPrice(auctionRequest.getPrice());
-//        auction.setTitle(auctionRequest.getTitle());
-//        auctionRepository.editAuction(auction);
-//    }
+    }
+
 }
